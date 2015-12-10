c8292a78fdf4754bdb3cf416fd662174b9870520
Mark Locker <marklocker@marks-mbp-3.lan>
2015-12-10 12:17:08 +0000

    Removed constructor from interface.

--- src/MapperInterface.php
+++ src/MapperInterface.php
@@ -4,17 +4,9 @@ namespace Air\Mapper;
 
 use Air\Database;
 use Doctrine\DBAL\Query\QueryBuilder;
-use Doctrine\Common\Collections\Collection;
 
 interface MapperInterface
 {
-    /**
-     * @param Database\ConnectionInterface $databaseConnection A database connection.
-     * @param Collection $collection A collection to store objects in.
-     */
-    public function __construct(Database\ConnectionInterface $databaseConnection, Collection $collection);
-
-
     /**
      * @return QueryBuilder A Doctrine QueryBuilder object.
      */
